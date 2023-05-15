73be01dd4010541bc90267e906b81adb64385d38
tony <tony@shilohsoftware.com>
2023-05-15 16:00:14 -0700

    Changes made to config file for deployment.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // This is the base path for your application.
+  basePath: '/',
 
-module.exports = nextConfig
+  // This is the environment that your application is running in.
+  env: {
+    NODE_ENV: 'production',
+  },
+
+  // This is the list of pages that should be exported when you run the `next export` command.
+  exportPathMap: {
+    '/pages/*': './pages/*',
+  },
+};
+
+module.exports = nextConfig;
