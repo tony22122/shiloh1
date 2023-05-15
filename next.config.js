0ad0a18f06f0e93952e156bb34fd51115b04b3bb
tony <tony@shilohsoftware.com>
2023-05-15 16:12:50 -0700

    Remove NODE ENV key from env config in next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 const nextConfig = {
   // This is the base path for your application.
-  basePath: '',
+  basePath: "",
 
   // This is the environment that your application is running in.
-  env: {
-    NODE_ENV: 'production',
-  },
+  env: {},
 
   // This is the list of pages that should be exported when you run the `next export` command.
   exportPathMap: async function () {
     return {
-      '/pages/*': { page: './pages/*' },
+      "/pages/*": { page: "./pages/*" },
     };
   },
 };
